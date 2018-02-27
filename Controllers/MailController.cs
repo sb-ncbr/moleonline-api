c0f0952baee7bf0768940f1a5051503e50290a78
Lukas Pravda <luky.pravda@gmail.com>
2018-02-27 23:07:47 +0000

    fix slow emails delivery

--- Controllers/MailController.cs
+++ Controllers/MailController.cs
@@ -30,37 +30,42 @@ namespace Mole.API.Controllers
 
             try
             {
-                SmtpClient client = new SmtpClient("smtp.gmail.com");
-                MailAddress from = new MailAddress(p.From, "MOLE API help", System.Text.Encoding.UTF8);
-                MailAddress to = new MailAddress("mole@upol.cz");
-
-
-                using (MailMessage message = new MailMessage(from, to))
+                using (SmtpClient client = new SmtpClient()
                 {
-                    message.Body =
-                        new string[] {
-                    "----------",
-                    "MOLE API mailing service\n",
-                    $"User {p.From} is asking for help with the following computation: https://mole.upol.cz/online/{p.ComputationId}/{p.SubmitId}",
-                    "DO NOT reply to this email directly and Respond ASAP :)",
-                    "----------\n",
-                    p.Msg,
-                    "\n",
-                    "Have a nice day =)",
-                    "MOLE API"
-                        }
-                        .Aggregate((a, b) => a + "\n" + b);
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new NetworkCredential("webchemistryhelp", "W3bCh3m.")
+                })
+                {
+                    MailAddress from = new MailAddress(p.From, "MOLE API help", System.Text.Encoding.UTF8);
+                    MailAddress to = new MailAddress("mole@upol.cz");
+
+                    using (MailMessage message = new MailMessage(from, to))
+                    {
+                        message.Body =
+                            new string[] {
+                                "MOLE API mailing service\n",
+                                $"User {p.From} is asking for help with the following computation: https://mole.upol.cz/online/{p.ComputationId}/{p.SubmitId}",
+                                "DO NOT reply to this email directly and Respond ASAP :)\n",
+                                "----------",
+                                p.Msg,
+                                "----------\n",
+                                "Have a nice day =)",
+                                "MOLE API"
+                            }
+                            .Aggregate((a, b) => a + "\n" + b);
 
 
-                    message.BodyEncoding = System.Text.Encoding.UTF8;
-                    message.Subject = $"MOLE API | {p.From} asks for help.";
-                    message.SubjectEncoding = System.Text.Encoding.UTF8;
+                        message.BodyEncoding = System.Text.Encoding.UTF8;
+                        message.Subject = $"MOLE API | {p.From} asks for help.";
+                        message.SubjectEncoding = System.Text.Encoding.UTF8;
 
-                    client.EnableSsl = true;
-                    client.Credentials = new NetworkCredential("webchemistryhelp", "W3bCh3m.");
-                    client.Send(message);
+                        client.Send(message);
+                    }
                 }
-
                 return Content(JsonConvert.SerializeObject(new { Success = true, Msg = "The message has been succesfully sent." }));
             }
             catch (Exception e)
